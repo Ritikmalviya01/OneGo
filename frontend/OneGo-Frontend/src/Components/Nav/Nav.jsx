bf953f4109b43af5d1983cee9598f191e8d2602f
Yuvraj Singh Thakur <132897291+yuvraj-thakur1311@users.noreply.github.com>
2025-02-16 22:52:17 +0530

    Update Nav.jsx

--- frontend/OneGo-Frontend/src/Components/Nav/Nav.jsx
+++ frontend/OneGo-Frontend/src/Components/Nav/Nav.jsx
@@ -1,11 +1,37 @@
-import React from "react";
-import navCSS from './../../Components/Nav/Nav.module.css'
+import React, { useRef } from 'react'
+import navCSS from './../Components/Nav/Nav.module.css'
 
-function Nav(){
-    return (
-        <div className={navCss.NAV_wrapper}>
-            <h1>Nav</h1>
+const Nav = () => {
+
+  const menu = useRef();
+  const MenHandler =()=> {
+    menu.current.classList.toggle(navCSS.activeMenu);
+  }
+  return (
+    <div className={navCSS.Nav_wrapper}>
+        <div className={navCSS.logo}>
+            <a href="#">ONEGO</a>
         </div>
-    )
+
+        <ul ref={menu}> 
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Transport</a></li>
+            <li><a href="#">AI BOT</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Search</a></li>
+        </ul>
+
+        <div className={navCSS.nav_btns}>
+            <div className={navCSS.search_wrapper}>
+                <i className='ri-search-line'/>
+                <input type='text' placeholder='Search Places'/>
+            </div>
+
+            <i className='ri-menu-2-line' onClick={MenHandler} id={navCSS.bars}/>
+        </div>
+    </div>
+  )
 }
+
 export default Nav
