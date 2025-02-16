6f79431b3b5f7c7d42c436642fdd4420010fd298
ritikmalviya40@gmail.com <ritikmalviya40@gmail.com>
2025-02-16 22:06:58 +0530

    first

---
+++ frontend/OneGo-Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Nav from './Components/Nav/Nav'
+
+function App() {
+  
+
+  return (
+    <div className='main'>
+     <Nav/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/OneGo-Frontend/src/Components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import navCSS from './../../Components/Nav/Nav.module.css'
+
+function Nav(){
+    return (
+        <div className={navCss.NAV_wrapper}>
+            <h1>Nav</h1>
+        </div>
+    )
+}
+export default Nav
