386d96d24ffc23accd1dbf8a9c3762ccaf8b6c7f
Yuvraj Singh Thakur <132897291+yuvraj-thakur1311@users.noreply.github.com>
2025-02-16 22:52:53 +0530

    Update main.jsx

--- frontend/OneGo-Frontend/src/main.jsx
+++ frontend/OneGo-Frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import './App.css'
+import Nav from './Components/Nav';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return (
+    
+    <div className='main'>
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
