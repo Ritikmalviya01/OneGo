81e419fc6e8c178b0d6af3a8607470aa93d6da92
ritikmalviya40@gmail.com <ritikmalviya40@gmail.com>
2025-02-18 20:50:17 +0530

    ok

--- frontend/OneGo-Frontend/src/App.jsx
+++ frontend/OneGo-Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
 import Nav from './Components/Nav/Nav';
+import Header from './Components/Header/Header';
 
 
 function App() {
   return (
     
     <div className='main'>
-      <Nav/>
+      <Nav />
+      <Header/>
     </div>
   )
 }
