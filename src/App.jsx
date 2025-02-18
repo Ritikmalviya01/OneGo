c9e7d69cb5c167c8ebbce9524cedb16fe0bcda6b
yuvraj-thakur1311 <yuvithakur1311@gmail.com>
2025-02-18 22:27:24 +0530

    5 commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Nav from './Components/Nav';
+
+function App() {
+  return (
+    
+    <div className='main'>
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
